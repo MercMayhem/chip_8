0125b01d2bee8c5e9087ac3cae1f3b0b4aa1f2d3
Aman Rao <amanrao032@gmail.com>
2023-02-13 12:55:46 +0530

    implemented some opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, fs::read};
 use crate::memory::Memory;
-use crate::opcode::Opcode;
+use crate::opcode::{Opcode, OpcodeTypes};
 
 struct Cpu{
     opcode : Opcode,
@@ -16,7 +16,7 @@ impl Cpu{
             panic!("Incorrect length of file")
         }
         for i in file.iter().enumerate(){
-            self.memory.addr_mem[i.0 + 512] = *i.1
+            self.memory.addr_mem[i.0 + 511] = *i.1
         }
     }
 
@@ -25,11 +25,63 @@ impl Cpu{
     }
 
     fn decode(&mut self){
-        let kind = Opcode::find_kind(self.opcode.code);
+        let kind = Opcode::find_kind(self.opcode.code).unwrap();
         self.opcode.kind = kind;
     }
 
-    fn execute(&self){
-        todo!()
+    fn execute(&mut self){
+        match self.opcode.kind{
+            OpcodeTypes::CLS => todo!(),
+            OpcodeTypes::RET => {
+                self.memory.pc = self.memory.stack[self.memory.sp as usize].expect("None in stack");
+                self.memory.sp -= 1;
+            },
+            OpcodeTypes::JPAddr => {
+                self.memory.pc = self.opcode.code & 0x0FFF;
+            },
+            OpcodeTypes::CALLAddr => {
+                self.memory.sp += 1;
+                self.memory.stack[self.memory.sp as usize] = Some(self.memory.pc)
+            },
+            OpcodeTypes::SEVxByte => {
+                let bytes = self.opcode.code.to_be_bytes();
+                let reg_no = bytes[0] & 0x0F;
+                let comp_val = bytes[1];
+
+                if self.memory.reg[reg_no as usize] == comp_val{
+                    self.memory.pc += 2;
+                }
+            },
+            OpcodeTypes::SNEVxByte => {
+                let bytes = self.opcode.code.to_be_bytes();
+                let reg_no = bytes[0] & 0x0F;
+                let comp_val = bytes[1];
+
+                if self.memory.reg[reg_no as usize] != comp_val{
+                    self.memory.pc += 2;
+                }
+            },
+            OpcodeTypes::SEVxVy => {
+                let bytes = self.opcode.code.to_be_bytes();
+                let reg1 = bytes[0] & 0x0F;
+                let reg2 = bytes[1].rotate_left(4) & 0x0F;
+
+                if self.memory.reg[reg1 as usize] == self.memory.reg[reg2 as usize]{
+                    self.memory.pc += 2;
+                }
+            },
+            OpcodeTypes::LDVxbyte => {
+                let bytes = self.opcode.code.to_be_bytes();
+                let reg_no = bytes[0] & 0x0F;
+
+                self.memory.reg[reg_no as usize] = bytes[1]
+            },
+            OpcodeTypes::ADDVxbyte => {
+                let bytes = self.opcode.code.to_be_bytes();
+                let reg_no = bytes[0] & 0x0F;
+
+                self.memory.reg[reg_no as usize] += bytes[1]
+            },
+        }
     }
 }
--- src/opcode.rs
+++ src/opcode.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, Debug)]
 pub enum OpcodeTypes{
     CLS,
     RET,
@@ -41,7 +42,174 @@ pub struct Opcode{
 }
 
 impl Opcode {
-    pub fn find_kind(opcode : u16) -> OpcodeTypes{
-        todo!()
+    pub fn find_kind(opcode : u16) -> Result<OpcodeTypes, String>{
+        if opcode == 0x00E0 {
+            Ok(OpcodeTypes::CLS)
+        }
+
+        else if opcode == 0x00EE {
+            Ok(OpcodeTypes::RET)
+        }
+
+        else if (opcode & 0xF000) == 0x1000{
+            Ok(OpcodeTypes::JPAddr)
+        }
+
+        else if (opcode & 0xF000) == 0x2000{
+            Ok(OpcodeTypes::CALLAddr)
+        }
+
+        else if (opcode & 0xF000) == 0x3000{
+            Ok(OpcodeTypes::SEVxByte)
+        }
+
+        else if (opcode & 0xF000) == 0x4000{
+            Ok(OpcodeTypes::SNEVxByte)
+        }
+
+        else if (opcode & 0xF000) == 0x5000{
+            Ok(OpcodeTypes::SEVxVy)
+        }
+
+        else if (opcode & 0xF000) == 0x6000{
+            Ok(OpcodeTypes::LDVxbyte)
+        }
+
+        else if (opcode & 0xF000) == 0x7000{
+            Ok(OpcodeTypes::ADDVxbyte)
+        }
+
+        else if (opcode & 0xF000) == 0x8000{
+            if (opcode & 0x000F) == 0x0000{
+                Ok(OpcodeTypes::LDVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0001{
+                Ok(OpcodeTypes::ORVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0002{
+                Ok(OpcodeTypes::ANDVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0003{
+                Ok(OpcodeTypes::XORVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0004{
+                Ok(OpcodeTypes::ADDVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0005{
+                Ok(OpcodeTypes::SUBVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0006{
+                Ok(OpcodeTypes::SHRVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x0007{
+                Ok(OpcodeTypes::SUBNVxVy)
+            }
+
+            else if (opcode & 0x000F) == 0x000E{
+                Ok(OpcodeTypes::SHLVxVy)
+            }
+
+            else {
+                Err("Incorrect Opcode".to_string())
+            }
+        }
+
+        else if (opcode & 0xF000) == 0x9000{
+            Ok(OpcodeTypes::SNEVxVy)
+        }
+
+        else if (opcode & 0xF000) == 0xA000{
+            Ok(OpcodeTypes::LDIAddr)
+        }
+
+        else if (opcode & 0xF000) == 0xB000{
+            Ok(OpcodeTypes::JPV0Addr)
+        }
+
+        else if (opcode & 0xF000) == 0xC000{
+            Ok(OpcodeTypes::RNDVxbyte)
+        }
+
+        else if (opcode & 0xF000) == 0xD000{
+            Ok(OpcodeTypes::DRWVxVyNibble)
+        }
+
+        else if (opcode & 0xF000) == 0xE000{
+            if (opcode & 0x00FF) == 0x00A1{
+                Ok(OpcodeTypes::SKNPVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x009E{
+                Ok(OpcodeTypes::SKPVx)
+            }
+
+            else{
+                Err("Incorrect Opcode".to_string())
+            }
+        }
+
+        else if (opcode & 0xF000) == 0xF000{
+            if (opcode & 0x00FF) == 0x0007{
+                Ok(OpcodeTypes::LDVxDT)
+            }
+
+            else if (opcode & 0x00FF) == 0x000A{
+                Ok(OpcodeTypes::LDVxK)
+            }
+
+            else if (opcode & 0x00FF) == 0x0015{
+                Ok(OpcodeTypes::LDDTVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x0018{
+                Ok(OpcodeTypes::LDSTVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x001E{
+                Ok(OpcodeTypes::ADDIVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x0029{
+                Ok(OpcodeTypes::LDFVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x0033{
+                Ok(OpcodeTypes::LDBVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x0055{
+                Ok(OpcodeTypes::LDIVx)
+            }
+
+            else if (opcode & 0x00FF) == 0x0065{
+                Ok(OpcodeTypes::LDVxI)
+            }
+
+            else{
+                Err("Incorrect Opcode".to_string())
+            }
+        }
+
+        else{
+            Err("Incorrect Opcode".to_string())
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::{OpcodeTypes, Opcode};
+
+    #[test]
+    fn eight_xy0(){
+        assert_eq!(OpcodeTypes::LDVxVy, Opcode::find_kind(0x8120).unwrap())
     }
 }
